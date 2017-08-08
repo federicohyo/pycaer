5e55a52f25b88b49b8ff68b21c3075b0a6f4aeea
Yuhuang Hu <duguyue100@gmail.com>
2017-08-08 11:54:39 +0200

    update test script

--- scripts/dvs128_example.py
+++ scripts/dvs128_example.py
@@ -3,6 +3,7 @@
 Author: Yuhuang Hu
 Email : duguyue100@gmail.com
 """
+from __future__ import print_function
 
 import pycaer.devices as devices
 
@@ -11,3 +12,13 @@ dvs128_handle = devices.dvs128(1, 0, 0, "")
 
 # Get device info
 dvs128_info = dvs128_handle.infoGet()
+
+print (dvs128_info.deviceID)
+print (dvs128_info.deviceString)
+print (dvs128_info.deviceIsMaster)
+print (dvs128_info.dvsSizeX)
+print (dvs128_info.dvsSizeY)
+print (dvs128_info.logicVersion)
+
+# send default configuration
+dvs128_handle.sendDefaultConfig()
