eb99b73b38bb9812ae80eb6f301b2ccac6f88899
Yuhuang Hu <duguyue100@gmail.com>
2017-08-08 12:45:38 +0200

    update dvs 128

--- scripts/dvs128_example.py
+++ scripts/dvs128_example.py
@@ -22,3 +22,14 @@ print (dvs128_info.logicVersion)
 
 # send default configuration
 dvs128_handle.sendDefaultConfig()
+
+# tweak some bias
+dvs128_handle.configSet(1, 11, 695)
+dvs128_handle.configSet(1, 10, 867)
+
+pr_bias = dvs128_handle.configGet(1, 11)
+foll_bias = dvs128_handle.configGet(1, 10)
+
+print (pr_bias, foll_bias)
+
+dvs128_handle.dataStart(None, None, None, None, None)
